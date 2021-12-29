7bfbe24f76e4b285ce1c8a18e83846121fb8ae96
richasinhaa <richa.sinha0603@gmail.com>
2021-12-29 12:35:46 +0800

    Indeed Job Portal Clone API Template

---
+++ src/main/java/com/api/indeedclone/IndeedCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.indeedclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndeedCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndeedCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/indeedclone/IndeedCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.indeedclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndeedCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
